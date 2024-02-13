2fed735518543a692619ad9ab6890e7d2b646ff4
Caroliveira <linaribeirooliveira@gmail.com>
2024-02-13 12:04:05 -0300

    Add custom success message to handle response

--- src/hooks/useHandleResponse.ts
+++ src/hooks/useHandleResponse.ts
@@ -8,17 +8,16 @@ type ResponseError = {
 const useHandleResponse = () => {
   const { setSnackbar } = useContext(GlobalContext);
 
-  const handleResponse = async <T extends ResponseError>(response: T) => {
+  const handleResponse = async <T extends ResponseError>(
+    response: T,
+    successMessage = "Success! Your changes have been saved."
+  ) => {
     if (response.error) {
       const message =
         response.error.message || "Something went wrong, try again later!";
       setSnackbar({ open: true, message, severity: "error" });
     } else {
-      setSnackbar({
-        open: true,
-        message: "Success! Your changes have been saved.",
-        severity: "success",
-      });
+      setSnackbar({ open: true, message: successMessage, severity: "success" });
     }
   };
 
